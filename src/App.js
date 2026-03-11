1e67d445c57e3f5b5234fd8e72fddc33d2e1f7a9
1-Kiran <kirankumarhosamani2@gmail.com>
2026-03-11 21:54:36 +0530

    Reset Button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    
+    return ( 
+        <div className="container">
+            <h1> { count } </h1>
+            <button onClick = {() => setCount(count + 1) } > Increase </button>
+            <button onClick = {() => setCount(count - 1) } > Decrease </button>
+            <button onClick = {() => setCount(count-count)}> Reset </button>
+        </div>
+    );
 }
 
 export default App;
