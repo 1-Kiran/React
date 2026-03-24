a682aa9fb2a5dc82188287e3d4d5dcfcf00ce320
1-Kiran <kirankumarhosamani2@gmail.com>
2026-03-24 12:16:37 +0530

    Project Completed

--- src/Notes-App/App.jsx
+++ src/Notes-App/App.jsx
@@ -18,6 +18,13 @@ function App() {
     console.log(copyList)
 
   }
+
+  const delteNote=(idx)=>{
+    const copyList=[...list]
+    copyList.splice(idx,1)
+
+    setList(copyList)
+  }
   return (
     <div className='h-screen bg-blackss text-white lg:flex'>
         <form onSubmit={(e)=>{
@@ -33,18 +40,20 @@ function App() {
             }} className='border-2 font-medium w-full h-35 py-2 px-5 mt-4 rounded' name="" placeholder='Enter Details' id=""></textarea>
             
             <button className=' bg-white text-black border-2 font-medium mt-4 w-full py-2 px-5 rounded'>Add Notes</button>
-      {/* <img className='w-50 h-50' src="https://tse3.mm.bing.net/th/id/OIP.bQCcCt3pT0lMkDVXgokyzQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" /> */}
       </form>
       <div className='lg:w-1/2 p-10 lg:border-l-1'>
         <h1 className='font-bold text-4xl'>Recent Notes </h1>
-        <div className='flex flex-wrap gap-5 mt-10 h-full overflow-auto'>
+        <div className='flex flex-wrap gap-5 mt-10 h-[90%] overflow-auto'>
         {list.map(function(elem,idx){
-          return <div key={idx} className="relative leading-fit h-50 w-40 rounded-2xl text-black bg-cover bg-[url('https://tse4.mm.bing.net/th/id/OIP.HiHK4Pa1BI9ovQXxBgBjygHaHy?rs=1&pid=ImgDetMain&o=7&rm=3')] overflow-auto">
+          return <div key={idx} className="relative flex items-start justify-between items-center flex-col leading-fit h-50 w-40 pb-4 p-1 rounded-2xl text-black bg-cover bg-[url('https://tse4.mm.bing.net/th/id/OIP.HiHK4Pa1BI9ovQXxBgBjygHaHy?rs=1&pid=ImgDetMain&o=7&rm=3')] overflow-auto">
+            <div>
             <h3 className='absolute top-7 right-5 bg-red-500 p-1 rounded-full text-xs'>
               <X size={16} color="#ffff" strokeWidth={2.77}/>
               </h3>
             <h3 className='font-bold text-xl text-center pt-8'>{elem.title}</h3>
-            <p className='pt-1'>{elem.details}</p>
+            <p className='mt-3 leading-tight font-medium text-gray-700'>{elem.details}</p>
+            </div>
+            <button onClick={delteNote} className='w-full text-xs text-white py-1  cursor-pointer active:scale-90 bg-red-600 rounded-2xl'>Delete</button>
           </div>
 
         })}
