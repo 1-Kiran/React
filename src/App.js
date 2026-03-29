8984245f568f25fa8d96ebf6cd6af04ae7e22af9
1-Kiran <kirankumarhosamani2@gmail.com>
2026-03-29 13:55:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import "./App.css"
+import Section1 from "./UI(Tailwind)/Section1";
 
 function App() {
     const [count, setCount] = useState(0);
     
     return ( 
-        <div className="container">
-            <h1> { count } </h1>
-            <button onClick = {() => setCount(count + 1) } > Increase </button>
-            <button onClick = {() => setCount(count - 1) } > Decrease </button>
-            <button onClick = {() => setCount(count-count)}> Reset </button>
-        </div>
+        // <div className="container">
+        //     <h1> { count } </h1>
+        //     <button onClick = {() => setCount(count + 1) } > Increase </button>
+        //     <button onClick = {() => setCount(count - 1) } > Decrease </button>
+        //     <button onClick = {() => setCount(count-count)}> Reset </button>
+        // </div>
+        <Section1 />
     );
 }
 
